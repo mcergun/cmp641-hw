896163cec69e886eb84d3d2442496d2b56dac948
Mert Can Ergun <mcergun@windowslive.com>
2016-12-19 22:57:01 +0300

    changed how thread count is captured

--- src/multiple2.c
+++ src/multiple2.c
@@ -9,19 +9,26 @@ extern int parallelism_enabled;
 void analyzeArray(ImageData *img, int y1, int y2, int x1, int x2);
 unsigned char assignDirection(ImageData *img, int y, int x);
 
+int thread_count = 6;
+int width = 640;
+int height = 512;
+
 int main(int argc, char *argv[])
 {
-	int width = 640;
-	int height = 512;
 	printf("asda\n");
-	if(argc < 3)
-		printf("running with standard 512x640 image generation\n");
-	else 
-	{
-		height = atoi(argv[1]);
-		width = atoi(argv[2]);
+	if(argc == 2) {
+		thread_count = atoi(argv[1]);
+		printf("setting thread num to %d\n", thread_count);	
+	} else if(argc > 3) {
+		thread_count = atoi(argv[1]);
+		height = atoi(argv[2]);
+		width = atoi(argv[3]);
 		printf("standard dimensions updated to %dx%d\n",
 			height, width);
+		printf("setting thread num to %d\n", thread_count);	
+	} else {
+		printf("running with standard 512x640 image generation\n");	
+		printf("setting thread num to %d\n", thread_count);	
 	}
 	// printf("parallelism_enabled = %d\n", parallelism_enabled);
 	ImageData *img = initializeImageData(height, width);
@@ -33,7 +40,7 @@ void analyzeArray(ImageData *img, int y1, int y2, int x1, int x2)
 {
 	for (int i = y1; i < y2; ++i)
 	{
-		#pragma omp parallel for
+		#pragma omp parallel for num_threads(thread_count)
 		for (int j = x1; j < x2; ++j)
 		{
 			assignDirection(img, i, j);
