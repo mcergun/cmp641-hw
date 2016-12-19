2e97e7bed40dc4b36e1e8731b66d70e2a7f3e3ed
Mert Can Ergun <mcergun@windowslive.com>
2016-12-19 22:57:20 +0300

    updated benchmark script to reflect code changes on sources

--- run_bench.sh
+++ run_bench.sh
@@ -1,11 +1,17 @@
 make > /dev/null
-export OMP_NUM_THREADS=6
-WIDTH=6400
-HEIGHT=5120
+WIDTH=10000
+HEIGHT=10000
+NUM_CORES=6
+echo "running test with randomly generated image of size $WIDTH x $HEIGHT"
 echo "single core"
 /usr/bin/time -f "Elapsed Time = %E" ./single $HEIGHT $WIDTH > /dev/null
-echo "multiple core1 cores=$OMP_NUM_THREADS"
-/usr/bin/time -f "Elapsed Time = %E" ./multiple1 $HEIGHT $WIDTH > /dev/null
-echo "multiple core2 cores=$OMP_NUM_THREADS"
-/usr/bin/time -f "Elapsed Time = %E" ./multiple2 $HEIGHT $WIDTH > /dev/null
+echo "multiple core1 cores=$NUM_CORES"
+/usr/bin/time -f "Elapsed Time = %E" ./multiple1 $NUM_CORES $HEIGHT $WIDTH > /dev/null
+echo "multiple core2 cores=$NUM_CORES"
+/usr/bin/time -f "Elapsed Time = %E" ./multiple2 $NUM_CORES $HEIGHT $WIDTH > /dev/null
+NUM_CORES=2
+echo "multiple core1 cores=$NUM_CORES"
+/usr/bin/time -f "Elapsed Time = %E" ./multiple1 $NUM_CORES $HEIGHT $WIDTH > /dev/null
+echo "multiple core2 cores=$NUM_CORES"
+/usr/bin/time -f "Elapsed Time = %E" ./multiple2 $NUM_CORES $HEIGHT $WIDTH > /dev/null
 echo "test finished"
