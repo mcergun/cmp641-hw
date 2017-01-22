dc57be6e18950d3b8cd04bc6eaa7ff6dc2db10fe
Mert Can Ergun <mcergun@windowslive.com>
2017-01-22 13:32:57 +0300

    new benchmark runner script

--- run_bench.sh
+++ run_bench.sh
@@ -2,15 +2,11 @@ make clean
 make
 WIDTH=5000
 HEIGHT=5000
-NUM_CORES=6
 ITERATIONS=10
-echo "running test with randomly generated image of size $WIDTH x $HEIGHT"
-echo "single core"
-./imganalyze $NUM_CORES $ITERATIONS 1 $HEIGHT $WIDTH
 NUM_CORES=2
-echo "multiple core1 cores=$NUM_CORES"
-./imganalyze $NUM_CORES $ITERATIONS 0 $HEIGHT $WIDTH
+./imganalyze $NUM_CORES
 NUM_CORES=4
-echo "multiple core1 cores=$NUM_CORES"
-./imganalyze $NUM_CORES $ITERATIONS 0 $HEIGHT $WIDTH
+./imganalyze $NUM_CORES
+NUM_CORES=6
+./imganalyze $NUM_CORES
 echo "test finished"
