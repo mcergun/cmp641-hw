2a78f8fbe3f18bc55c754b612025bde92157b17b
Mert Can Ergun <mcergun@windowslive.com>
2017-01-22 13:19:56 +0300

    removed unnecessary functions

--- src/imagehelper.h
+++ src/imagehelper.h
@@ -15,15 +15,8 @@ ImageData * initializeImageData(int height, int width);
 ImageData * initializeEmptyImageData(int height, int width);
 void destroyImageData(ImageData * img);
 
-void assignDirection(ImageData *img, ImageData *dest, int y, int x);
-void sumAndCompare(ImageData *img, ImageData *dest, int y, int x, int limit);
-
 void printResult(double val);
 
-double analyzeArrayS(ImageData *src, ImageData *dest, ImageData *dest2);
-double analyzeArrayM1(ImageData *src, ImageData *dest, ImageData *dest2);
-double analyzeArrayM1wC(ImageData *src, ImageData *dest, ImageData *dest2);
-double analyzeArrayM2(ImageData *src, ImageData *dest, ImageData *dest2);
 int compareImages(ImageData *img1, ImageData *img2);
 
 #endif
