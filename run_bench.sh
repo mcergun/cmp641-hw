87f473b6354e5392fce1177791daef9b536870f8
Mert Can Ergun <mcergun@windowslive.com>
2017-01-22 20:27:50 +0300

    benchmark script updated

--- run_bench.sh
+++ run_bench.sh
@@ -4,11 +4,13 @@ WIDTH=5000
 HEIGHT=5000
 ITERATIONS=10
 NUM_CORES=1
-./imganalyze $NUM_CORES
+./imganalyze $NUM_CORES 1
 NUM_CORES=2
-./imganalyze $NUM_CORES
+./imganalyze $NUM_CORES 0
 NUM_CORES=4
-./imganalyze $NUM_CORES
+./imganalyze $NUM_CORES 0
 NUM_CORES=6
-./imganalyze $NUM_CORES
+./imganalyze $NUM_CORES 0
+NUM_CORES=8
+./imganalyze $NUM_CORES 0
 echo "test finished"
